53e483c62b2924e96b49331040b799c437f257f8
Mark Studer <mark@beyondcomputing.org>
2019-11-05 20:13:57 -0700

    Updating build

--- Build.ps1
+++ Build.ps1
@@ -5,14 +5,17 @@ Install-Module PSScriptAnalyzer -Force
 # Run Unit Tests
 Invoke-Pester -EnableExit -OutputFile 'TestResults.xml' -OutputFormat NUnitXml
 
+# Get Module Name
+$ModuleName = (Get-Item '*.psd1').Name.Replace('.psd1','')
+
 # Create Build Folder
-$BuildFolder = 'Build/AWS.SAML'
+$BuildFolder = "Build/$ModuleName"
 if(!(Test-Path $BuildFolder)){
     New-Item -ItemType Directory -Path $BuildFolder
 }
 
 # Copy in Items for Release
-Copy-Item -Path 'AWS.SAML.psd1' -Destination $BuildFolder -Force
+Copy-Item -Path "$ModuleName.psd1" -Destination $BuildFolder -Force
 Copy-Item -Path 'README.md' -Destination $BuildFolder -Force
 Copy-Item -Path 'CHANGELOG.md' -Destination $BuildFolder -Force
 Copy-Item -Path 'LICENSE.md' -Destination $BuildFolder -Force
